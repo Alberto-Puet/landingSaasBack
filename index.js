40c5d8692e54b7e9fa2be46f741c7814b1661ca8
Alberto <albertopuet8@gmail.com>
2026-03-06 01:59:02 -0300

    landing saas back

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ app.post('/generate-diagnostic', async (req, res) => {
     }
 });
 
-const PORT = 3000;
-app.listen(PORT, () => {
-    console.log(`🚀 SERVIDOR ESCUCHANDO EN http://localhost:${PORT}`);
+const PORT = process.env.PORT || 3000; 
+app.listen(PORT, "0.0.0.0", () => { 
+    console.log(`🚀 SERVIDOR ESCUCHANDO EN EL PUERTO: ${PORT}`);
 });
